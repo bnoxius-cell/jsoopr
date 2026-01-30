43814791b2c2060898a46b0d7fbcbe478b778d93
bnoxius-cell <idevadrian@gmail.com>
2026-01-30 08:04:21 +0800

    yes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/header/Header';
+import Banner2 from './components/banner2/Banner2';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      </div>
+  );
+}
+
+export default App;
